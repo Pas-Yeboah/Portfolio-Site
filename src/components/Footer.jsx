9db74a8e412c4e4039ff5b06a3f6fcecfe6edf1b
pas-yeboah <philipay65@gmail.com>
2026-02-26 17:57:36 +0000

    feat: Integrate lucide-react icons in Footer component and update email icon size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Github} from 'lucide-react'
 
 const Footer = () => {
   return (
@@ -12,8 +13,9 @@ const Footer = () => {
          <a href="https://www.linkedin.com/in/philipa-yeboah/" target='_blank'className='text-zinc-500 hover:text-zinc-900'>Connect with me on LinkedIn</a>
        </div>
         <div className='flex justify-center items-center'>
-          <img src="https://img.icons8.com/ios/50/gmail--v1.png" className='w-8 h-8' alt="" />
+          <img src="https://img.icons8.com/ios/25/gmail--v1.png"  alt="" />
           <a href="mailto:philipay65@gmail.com" target='_blank' className='text-zinc-500 hover:text-zinc-900'>Send a mail</a>
+          {/* <Github className='' href='https://github.com/Pas-Yeboah'/> */}
         </div>
     </footer>
   )
